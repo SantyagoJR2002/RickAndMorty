036fcee4f79e0382fd54ad1f98d733f5f57161cb
SantyagoJR2002 <96457958+SantyagoJR2002@users.noreply.github.com>
2022-06-13 23:52:07 -0500

    Update Search.jsx

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -9,10 +9,12 @@ setSearch(e.target.children[0].value)
    }
    
     return(
+       <div className="u">
         <form onSubmit={searching}>
         <input type="text"/>
 <button>Search</button>
         </form>
+</div>
     )
 }
 export default Search
